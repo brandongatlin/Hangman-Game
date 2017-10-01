b38de441b80b5e513ddd63860b2fc5971bd0fa5f
brandongatlin <brandongatlin1981@me.com>
2017-10-01 00:18:37 -0500

    added letter array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,12 @@ var wordList = [
 			["animales", "gato", "perro", "serpiente"],
 			["comida", "manzana", "limon", "cereza"],
 			["escuela", "lapiz", "goma", "papel"]
-]
+];
 
     console.log(wordList[1][3]);
 
+var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+	console.log(letterChoices[7]);
 
-
-
+            
