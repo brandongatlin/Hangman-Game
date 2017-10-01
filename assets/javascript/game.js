d17b1473b7197e9a7d23cc93593c2d8878837c96
brandongatlin <brandongatlin1981@me.com>
2017-10-01 13:14:59 -0500

    added enne

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,41 @@
-var wordList = [
-			["animales", "gato", "perro", "serpiente"],
-			["comida", "manzana", "limon", "cereza"],
-			["escuela", "lapiz", "goma", "papel"]
-];
+var wordList = ["animales", "gato", "perro", "serpiente", "comida", "manzana", "limon", "cereza", "escuela", "lapiz", "goma", "papel"];
 
-    console.log(wordList[1][3]);
+    console.log(wordList[3]);
 
-var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "\u00f1", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-	console.log(letterChoices[7]);
+	console.log(letterChoices[14]);
 
-            
+// canvas drawing
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// draw ground
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.moveTo(50,250);
+ctx.lineTo(150,250);
+ctx.lineTo(150,240);
+ctx.lineTo(50,240);
+ctx.closePath();
+ctx.stroke();
+ctx.fillStyle = "black";
+ctx.fill();
+
+
+
+// draw gallows pole
+
+
+
+// draw hanging pole
+
+
+
+// draw brace
+
+
+
+
+
+// test
