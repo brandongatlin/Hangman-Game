f9839ae35e560786658902e95f0f8f99e18873f8
brandongatlin <brandongatlin1981@me.com>
2017-10-01 00:03:34 -0500

    made word arrays

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var wordList = [
+			["animales", "gato", "perro", "serpiente"],
+			["comida", "manzana", "limon", "cereza"],
+			["escuela", "lapiz", "goma", "papel"]
+]
+
+    console.log(wordList[1][3]);
+
+
+
+
+
